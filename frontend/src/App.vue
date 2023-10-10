b2d5f625d143d38c915e7ff6d4dab8cf6887362b
alopez <alopez@tecnosylva.com>
2023-10-10 16:24:01 +0200

    feat(front): added basic vue + vite front - added open layers map

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Map from './components/Map.vue'
+</script>
+
+<template>
+  <div id="map">
+    <Map/>
+  </div>
+</template>
+
+<style scoped>
+#map {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+}
+</style>
---
+++ frontend/src/components/Map.vue
@@ -0,0 +1,36 @@
+<template>
+  <ol-map
+      :loadTilesWhileAnimating="true"
+      :loadTilesWhileInteracting="true"
+      style="height: 100%"
+  >
+    <ol-view
+        ref="view"
+        :center="center"
+        :rotation="rotation"
+        :zoom="zoom"
+        :projection="projection"
+    />
+
+    <ol-tile-layer>
+      <ol-source-osm />
+    </ol-tile-layer>
+
+    <ol-vector-layer>
+      <ol-source-vector :url="url" :format="geoJson"> </ol-source-vector>
+    </ol-vector-layer>
+  </ol-map>
+</template>
+
+<script setup>
+import {inject, ref} from "vue";
+
+const center = ref([40, 40]);
+const projection = ref("EPSG:4326");
+const zoom = ref(8);
+const rotation = ref(0);
+
+const url = ref("https://raw.githubusercontent.com/codeforgermany/click_that_hood/main/public/data/spain-provinces.geojson");
+const format = inject("ol-format");
+const geoJson = new format.GeoJSON();
+</script>
